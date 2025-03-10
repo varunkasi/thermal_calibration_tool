6fea6d9929e0b442edc57844a73e6a076b8bd866
varunkasi <vkasired@alumni.cmu.edu>
2025-03-10 04:26:36 -0400

    small bug related to ros2 sourcing in run_thermal_calibration.sh is fixed.

--- scripts/run_thermal_calibration.sh
+++ scripts/run_thermal_calibration.sh
@@ -349,8 +349,6 @@ else
     log_info "Skipping package build as requested."
 fi
 
-# Define ROS source command that ensures both ROS and workspace are sourced
-ROS_SOURCE_CMD=". /opt/ros/humble/setup.bash && ${WORKSPACE_SETUP_CMD}"
 
 # Create the tmux session for running multiple commands
 log_info "Setting up tmux session for thermal calibration..."
@@ -403,8 +401,7 @@ else
         log_info "Starting thermal calibration rqt plugin..."
         PERSPECTIVE_PATH="${CONTAINER_WORKSPACE_DIR}/install/thermal_calibration_rqt/share/thermal_calibration_rqt/resource/thermal_calibration.perspective"
         # Check if perspective file exists
-docker exec $CONTAINER_NAME bash -c "tmux send-keys -t thermal_calibration:3 '${ROS_SOURCE_CMD} rqt --standalone thermal_calibration_rqt.thermal_calibration_plugin.ThermalCalibrationPlugin' C-m"
-
+        docker exec $CONTAINER_NAME bash -c "tmux send-keys -t thermal_calibration:3 '${ROS_SOURCE_CMD} rqt --standalone thermal_calibration_rqt.thermal_calibration_plugin.ThermalCalibrationPlugin' C-m"
     else
         log_info "Starting standard rqt (thermal calibration plugin not installed)..."
         docker exec -it $CONTAINER_NAME bash -c "tmux send-keys -t thermal_calibration:3 '${ROS_SOURCE_CMD} rqt' C-m"
